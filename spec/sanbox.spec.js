5dfb1629112717511cf4fc03684b87862b2c76a2
Girma Nigusse <xgirma@gmail.com>
2020-01-25 17:02:34 -0500

    update tests

--- spec/sanbox.spec.js
+++ spec/sanbox.spec.js
@@ -8,27 +8,17 @@ const chromeOptions = process.env.GITHUB_ACTIONS ? options.headless() : options;
 
 describe("Sandbox", () => {
   let browser;
-  let originalTimeout;
 
   beforeAll(async () => {
     browser = await new Builder()
       .forBrowser("chrome")
       .setChromeOptions(chromeOptions)
       .build();
-    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+    await browser.get("https://e2e-boilerplates.github.io/sandbox/");
   });
 
   afterAll(() => {
     browser.quit();
-  });
-
-  beforeEach(() => {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
-  });
-
-  afterEach(() => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
 
   it("should be on Sandbox", async () => {
